8ceef468dc1bd895fc081c6f7660967ddcc1b1ba
Dillon Ward <dillon.ward.uk@gmail.com>
2020-01-28 16:15:58 +0000

    Platformer presets set as a library

---
+++ Pygame Projects/Vertical platformer/platformer_presets.py
@@ -0,0 +1,10 @@
+import pygame
+
+BLACK    = (   0,   0,   0)
+WHITE    = ( 255, 255, 255)
+BLUE     = (   0,   0, 255)
+RED      = ( 255,   0,   0)
+GREEN    = (   0, 255,   0)
+
+SCREEN_WIDTH  = 800
+SCREEN_HEIGHT = 600
